491ef06393cd62520c2b265e6773140336b6e28b
daeun084 <030804jk@naver.com>
2023-04-04 14:19:51 +0900

    Chapter4

--- Chapter2/src/Q12.java
+++ Chapter2/src/Q12.java
@@ -10,6 +10,26 @@ public class Q12 {
         int n2 = sc.nextInt();
 
 
+        switch (x){
+            case("+"):
+                System.out.printf("%d%s%d의 계산 결과는 %1d\n", n1, x, n2, n1+n2);
+                break;
+            case("-"):
+                System.out.printf("%d%s%d의 계산 결과는 %d\n", n1, x, n2, n1-n2);
+                break;
+            case ("*"):
+                System.out.printf("%d%s%d의 계산 결과는 %d\n", n1, x, n2, n1*n2);
+                break;
+            case("/"):
+                if(n2==0){
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return;  //종료
+                }
+                System.out.printf("%d%s%d의 계산 결과는 %d\n", n1, x, n2, n1/n2);
+
+        }
+
+
         //if-else문
 /*
         if(x.equals("+")){
@@ -30,24 +50,7 @@ public class Q12 {
         }
 */
 
-        //switch문
-        switch (x){
-            case("+"):
-                System.out.printf("%d%s%d의 계산 결과는 %1d\n", n1, x, n2, n1+n2);
-                break;
-            case("-"):
-                System.out.printf("%d%s%d의 계산 결과는 %d\n", n1, x, n2, n1-n2);
-                break;
-            case ("*"):
-                System.out.printf("%d%s%d의 계산 결과는 %d\n", n1, x, n2, n1*n2);
-                break;
-            case("/"):
-                if(n2==0){
-                    System.out.println("0으로 나눌 수 없습니다.");
-                    return;  //종료
-                }
-                System.out.printf("%d%s%d의 계산 결과는 %d\n", n1, x, n2, n1/n2);
 
-        }
+
     }
 }

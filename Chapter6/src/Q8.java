ce3a4b42d7087aa837c08e0a6c66c5023fe9f5e2
daeun084 <030804jk@naver.com>
2023-05-23 16:44:28 +0900

    .

---
+++ Chapter6/src/Q8.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args){
+        System.out.println("문자열을입력하세요. 빈칸이나 있어도 되고 영어 한글 모두 됩니다.");
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+    }
+}
---
+++ HomeWork3/src/Main.java
@@ -0,0 +1,10 @@
+//자료구조 과제3
+
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = new int[32];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = (int)(Math.random()*31);
+        }
+    }
+}

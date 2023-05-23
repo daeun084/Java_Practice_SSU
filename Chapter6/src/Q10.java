39d36c5c874e52201cb4a239c51d8bb37e7b28c3
daeun084 <030804jk@naver.com>
2023-05-23 20:26:54 +0900

    Chapter6

--- Chapter6/src/Q10.java
+++ Chapter6/src/Q10.java
@@ -2,30 +2,66 @@ import java.util.Scanner;
 
 class Person{
     private String name;
-   public void getname(String name){
+   public void setname(String name){
       this.name = name;
+   }
+   public String getName(){
+       return name;
+   }
+   int a, b, c;
+   int win(){
+       if(a==b& b==c) {
+           System.out.println("\t"+name+"님이 이겼습니다!");
+           return 1;
+       }
+       else {
+           System.out.println("\t아쉽군요!");
+           return 0;
+       }
    }
 
 }
 public class Q10 {
+    static Scanner sc = new Scanner(System.in);
+    static void run(Person [] parr){
+        int index = -1;
+        sc.nextLine();
+        String ent;
+        int win=0;
 
-    static void run(Person a, Person b){
         while(true){
-           // System.out.print();
+            index++;
+          System.out.print("["+parr[index].getName()+"] : ");
+          ent = sc.nextLine();
+
+          win = makerandom(parr[index]);
+
+            if(win==1) return;
+          if(index==1) index = -1;
         }
 
     }
+
+    static int makerandom(Person p){
+        p.a = (int)(Math.random()*3 + 1);
+        p.b = (int)(Math.random()*3 + 1);
+        p.c = (int)(Math.random()*3 + 1);
+
+        System.out.print("\t\t"+p.a +"\t"+p.b+"\t"+p.c);
+
+        return p.win();
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         Person a = new Person();
         Person b = new Person();
+        Person[] parr = {a, b};
 
         System.out.print("1번째 선수 이름>>");
-        a.getname(sc.next());
+        a.setname(sc.next());
         System.out.print("2번째 선수 이름>>");
-        b.getname(sc.next());
+        b.setname(sc.next());
 
-        run(a, b);
+        run(parr);
     }
 }
--- Chapter6/src/Q12.java
+++ Chapter6/src/Q12.java
@@ -1,12 +1,53 @@
 import java.util.Scanner;
 
 public class Q12 {
+    static Scanner sc = new Scanner(System.in);
+
+    static void run(Person[] parr, int num){
+        int index = -1;
+        sc.nextLine();
+        String ent;
+        int win=0;
+
+        while(true){
+            index++;
+            System.out.print("["+parr[index].getName()+"] : ");
+            ent = sc.nextLine();
+
+            win = makerandom(parr[index]);
+
+            if(win==1) return;
+            if(index==num-1) index = -1;
+        }
+
+    }
+
+    static int makerandom(Person p){
+        p.a = (int)(Math.random()*3 + 1);
+        p.b = (int)(Math.random()*3 + 1);
+        p.c = (int)(Math.random()*3 + 1);
+
+        System.out.print("\t\t"+p.a +"\t"+p.b+"\t"+p.c);
+
+        return p.win();
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         System.out.print("겜블링 게임에 참여할 선수 숫자>>");
         int num = sc.nextInt();
 
+        Person[] parr = new Person[num];
+        for(int i=0; i<num; i++){
+            parr[i] = new Person();
+            System.out.print(i+1 +"번째 선수 이름>>");
+            parr[i].setname(sc.next());
+        }
 
-
+        run(parr, num);
     }
 }
+
+
+
+
+
